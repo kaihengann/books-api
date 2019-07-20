3638dfbe21be935f2a0207d34a08e171f7288b2c
kaihengann <annkaiheng@gmail.com>
2019-07-20 18:49:23 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.route("/").get((req, res) => {
-  res.send("Hello there!");
+  res.send("Hello again!");
 });
 
 module.exports = router;
